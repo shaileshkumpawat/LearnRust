940f34e0f45a7d58181199db610e0a540f157149
ShaileshKumpawat <shaileshkumpawat@gmail.com>
2024-02-07 01:36:46 +0530

    Iter implemented in second.rs

--- projects/lists/src/second.rs
+++ projects/lists/src/second.rs
@@ -2,11 +2,6 @@
 
 pub struct IntoIter<T>(List<T>);
 
-pub struct Iter<'a, T> {
-    next: Option<&'a Node<T>>
-}
-
-
 pub struct List<T> {
     head: Link<T>
 }
@@ -73,11 +68,37 @@ impl<T> List<T> {
     }
 }
 
-impl<'a, T> List<T> {
-    pub fn iter(&'a self) -> Iter<'a, T> {
-        Iter { next: self.head.map(|node| &'a node) }
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.head.as_deref() }
     }
 }
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+#[test]
+fn iter() {
+    let mut list = List::new();
+    list.push(1); list.push(2); list.push(3);
+
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&1));
+}
+
 
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
@@ -86,17 +107,6 @@ impl<T> Iterator for IntoIter<T> {
         self.0.pop()
     }
 }
-
-impl<'a, T> Iterator for Iter<'a, T> {
-    type Item = &'a T;
-    fn next(&'a mut self) -> Option<Self::Item> {
-        self.next.map(|node| {
-            self.next = node.next.map(|node| &'a node);
-            &'a node.elem
-        })
-    }
-}
-
 #[test]
 fn peek() {
     let mut list = List::new();
